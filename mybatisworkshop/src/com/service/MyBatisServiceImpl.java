c658bc8868058204b3fe408513fc8b6fad6d6c61
Kim Bohee <boingkimm@gmail.com>
2023-08-10 22:17:36 +0900

    01_학생정보조회

---
+++ mybatisworkshop/src/com/service/MyBatisServiceImpl.java
@@ -0,0 +1,36 @@
+package com.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.config.MySqlSessionFactory;
+import com.dao.MyBatisDAO;
+import com.dto.StudentDTO;
+
+public class MyBatisServiceImpl implements MyBatisService {
+
+	@Override
+	public List<StudentDTO> selectAllStudent() {
+		List<StudentDTO> list = null;
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+		//DAO 연동
+		 MyBatisDAO dao = new MyBatisDAO();
+		 list = dao.selectAllStudent(session);
+		}finally {
+			session.close();
+		}
+		return list;
+	}
+} //MyBatisServiceImpl
+
+
+/*
+ * SqlSession session = MySqlSessionFactory.getSession();
+		try{
+		
+		}finally {
+			session.close();
+		}
+*/
